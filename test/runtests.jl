c5e575730cfaa9be0c5530d188d3aadb5a0dccb5
Adrian Lanzafame <lanzafame@192-168-1-3.tpgi.com.au>
2015-07-23 21:49:21 +1000

    Reorder using calls in test file

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,7 @@
 using Connect
-using Base.Test
-Pkg.add("JSON")
-Pkg.add("HTTPClient")
 using JSON
 using HTTPClient.HTTPC
+using Base.Test
 
 # write your own tests here
 connect = ConnectClient("55b078611f2ddd01a84c28db", "82B8B12421E674E10309A2B926BCFEB2-9F2ECFA4B7353269DE3BB515DFB8E3A4A754B4D36145CA2EB3554C9BF985C45C204736841691A9138D00CA88FE15A165")
